4b2f86c25b93aba88ad3024fed108c7cb90fa12f
Indra Gunawan <feedsbrain@gmail.com>
2020-10-22 14:31:37 +0800

    Change development port

--- src/config/development.ts
+++ src/config/development.ts
@@ -1,7 +1,7 @@
 import { Config } from './index'
 
 const config: Config = {
-  port: 3000,
+  port: 3006,
   secret: process.env.API_SECRET || '',
   api: {
     url: 'https://mysecurity.eufylife.com/api/v1',
